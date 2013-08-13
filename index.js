7dcdb2e1f1f40d0e278a338572ce09b95549b847
Michael Trotter <spicydonuts@gmail.com>
2013-08-13 10:29:56 -0600

    finished initial implementation and added tests

--- index.js
+++ index.js
@@ -7,34 +7,42 @@ var pkg = require('./package')
 ;
 
 module.exports = function(appname, env, keydir, privsuffix, pubsuffix) {
+  
+  function pair(tcid, priv, pub) {
+    var key = { priv: appname === tcid ? priv : '', pub: pub };
+    keycache[tcid] = key;
+    return key;
+  }
+
+  function clean(path) {
+    path = path || '/';
+    return path[0] === '/' ? path : '/'.concat(path);
+  }
+
+  appname = clean(appname);
   privsuffix = privsuffix || 'key';
   pubsuffix = pubsuffix || 'key.pub';
   env = env || 'dev';
   keydir = path.join(keydir || path.join(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE, '.ns'), env);
   return function(tcid, cb) {
+    tcid = clean(tcid);
     if (tcid in keycache) {
       return (cb && cb(null, keycache[tcid])) || keycache[tcid];
     }
     if (cb) {
-      return fs.readFile(path.join(process.env.HOME, tcid, privsuffix), { encoding: 'utf8' }, function(priverr, priv) {
+      return fs.readFile(path.join(keydir, tcid, privsuffix), { encoding: 'utf8' }, function(priverr, priv) {
         if (priverr) return cb(priverr);
-        return fs.readFile(path.join(process.env.HOME, tcid, pubsuffix), { encoding: 'utf8' }, function(puberr, pub) {
+        return fs.readFile(path.join(keydir, tcid, pubsuffix), { encoding: 'utf8' }, function(puberr, pub) {
           if (puberr) return cb(puberr);
           return cb(null, pair(tcid, priv, pub));
         });
       });
     }
-    var priv = fs.readFileSync(path.join(process.env.HOME, tcid, 'key'), { encoding: 'utf8' })
-    , pub = fs.readFileSync(path.join(process.env.HOME, tcid, 'key.pub'), { encoding: 'utf8' })
+    var priv = fs.readFileSync(path.join(keydir, tcid, 'key'), { encoding: 'utf8' })
+    , pub = fs.readFileSync(path.join(keydir, tcid, 'key.pub'), { encoding: 'utf8' })
     ;
     return pair(tcid, priv, pub);
   };
 };
-
-function pair(tcid, priv, pub) {
-  var key = { priv: appname === tcid ? priv || '', pub: pub };
-  keycache[tcid] = key;
-  return key;
-}
 
 Object.defineProperty(module.exports, 'version', { enumerable: true, value: pkg.version });
--- test/test.js
+++ test/test.js
@@ -1,2 +1,31 @@
 'use strict';
 
+var expect = require('expect.js')
+
+, env      = 'dev'
+, app      = 'test'
+, keys     = require('../')(app, env)
+;
+
+describe('key-manager', function() {
+
+	it('returns keys synchronously', function() {
+		expect(keys(app).pub).to.be.ok();
+	});
+
+	it('returns keys asynchronously', function(done) {
+		keys(app, function(err, res) {
+			expect(err).to.not.be.ok();
+			expect(res).to.be.ok();
+			done();
+		});
+	});
+
+	it('returns private keys when the tcid matches the app name', function() {
+		expect(keys(app).priv).to.be.ok();
+	});
+
+	it('returns nothing when the tcid does not match the app name', function() {
+		expect(keys('claims-authority').priv).to.not.be.ok();
+	});
+});
